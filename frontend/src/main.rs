683168fca34926450b83314ffa1bb3851e317b13
Ramon Klass <tier@schokokeks.org>
2022-11-04 15:57:13 +0100

    send slider data per slider every frame (only if changed)
    previous behavior was to send on every input event which was too spammy

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -7,15 +7,17 @@ macro_rules! console_log {
 pub mod state;
 pub mod websocket;
 
-use futures::{executor::block_on, SinkExt};
+use futures::{executor::block_on, SinkExt, StreamExt};
+use gloo_timers::future::IntervalStream;
 use protocol::ClientMessage;
 use state::AppState;
-use sycamore::prelude::*;
+use sycamore::{futures::spawn_local_scoped, prelude::*};
 use web_sys::Event;
 
 #[component(inline_props)]
 fn Slider<G: Html>(cx: Scope, slider_id: usize, slider: RcSignal<u8>) -> View<G> {
     let slider = create_ref(cx, slider);
+    let slider_id = create_ref(cx, slider_id);
     let stringy = create_signal(cx, String::new());
     // updating the string
     create_effect(cx, || {
@@ -25,16 +27,31 @@ fn Slider<G: Html>(cx: Scope, slider_id: usize, slider: RcSignal<u8>) -> View<G>
     });
     let state = use_context::<AppState>(cx);
     // updating the number
+    let new_value = create_signal(cx, None);
     create_effect(cx, move || {
         let value = str::parse::<u8>(&stringy.get()).unwrap_or_default();
         slider.set(value);
-        let mut sender = state.sender.clone();
-        block_on(async move {
-            sender
-                .send(serde_json::to_string(&ClientMessage::Update(slider_id, value)).unwrap())
-                .await
-                .unwrap();
-        });
+        new_value.set(Some(value));
+    });
+    spawn_local_scoped(cx, async {
+        IntervalStream::new(15)
+            .for_each(|_| async {
+                if let Some(value) = *new_value.get() {
+                    let mut sender = state.sender.clone();
+                    sender
+                        .send(
+                            serde_json::to_string(&ClientMessage::Update(
+                                slider_id.to_owned(),
+                                value,
+                            ))
+                            .unwrap(),
+                        )
+                        .await
+                        .unwrap();
+                    new_value.set(None);
+                }
+            })
+            .await;
     });
     view! { cx,
         div(class="inner slidecontainer") {
